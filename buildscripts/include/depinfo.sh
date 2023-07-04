49aa8acfd47709763bec5454e20ed8835e224aa8
Hitesh Kumar Saini <saini123hitesh@gmail.com>
2023-07-05 00:45:02 +0530

    Update depinfo.sh

--- buildscripts/include/depinfo.sh
+++ buildscripts/include/depinfo.sh
@@ -7,10 +7,6 @@ v_ndk=25.1.8937393
 v_sdk_build_tools=33.0.1
 
 v_lua=5.2.4
-v_libass=0.17.0
-v_harfbuzz=7.0.0
-v_fribidi=1.0.12
-v_freetype=2-13-0
 v_mbedtls=3.3.0
 v_libplacebo=5.229.2
 v_dav1d=1.1.0
@@ -24,13 +20,9 @@ v_mpv=0.35.1
 dep_mbedtls=()
 dep_dav1d=()
 dep_ffmpeg=(mbedtls dav1d)
-dep_freetype2=()
-dep_fribidi=()
-dep_harfbuzz=()
-dep_libass=(freetype fribidi harfbuzz)
 dep_lua=()
 dep_shaderc=()
 dep_libplacebo=(shaderc)
-dep_mpv=(ffmpeg libass lua libplacebo)
+dep_mpv=(ffmpeg lua libplacebo)
 dep_mpv_android=(mpv)
 
