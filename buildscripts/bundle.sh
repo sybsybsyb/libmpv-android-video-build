88d86e7ff8ffb2f7e4df324a63bcbd82d976bde7
Hitesh Kumar Saini <saini123hitesh@gmail.com>
2023-07-09 18:24:36 +0530

    fix: bundle.sh

--- buildscripts/bundle.sh
+++ buildscripts/bundle.sh
@@ -1,29 +1,30 @@
 # --------------------------------------------------
 
-cd media-kit-android-helper
+cd deps/media-kit-android-helper
 
 sudo chmod +x gradlew
 ./gradlew assembleRelease
 
 unzip -o app/build/outputs/apk/release/app-release.apk -d app/build/outputs/apk/release
 
-ln -sf "$(pwd)/app/build/outputs/apk/release/lib/arm64-v8a/libmediakitandroidhelper.so" "../../libmpv/src/main/jniLibs/arm64-v8a"
-ln -sf "$(pwd)/app/build/outputs/apk/release/lib/armeabi-v7a/libmediakitandroidhelper.so" "../../libmpv/src/main/jniLibs/armeabi-v7a"
-ln -sf "$(pwd)/app/build/outputs/apk/release/lib/x86/libmediakitandroidhelper.so" "../../libmpv/src/main/jniLibs/x86"
-ln -sf "$(pwd)/app/build/outputs/apk/release/lib/x86_64/libmediakitandroidhelper.so" "../../libmpv/src/main/jniLibs/x86_64"
+ln -sf "$(pwd)/app/build/outputs/apk/release/lib/arm64-v8a/libmediakitandroidhelper.so" "../../../libmpv/src/main/jniLibs/arm64-v8a"
+ln -sf "$(pwd)/app/build/outputs/apk/release/lib/armeabi-v7a/libmediakitandroidhelper.so" "../../../libmpv/src/main/jniLibs/armeabi-v7a"
+ln -sf "$(pwd)/app/build/outputs/apk/release/lib/x86/libmediakitandroidhelper.so" "../../../libmpv/src/main/jniLibs/x86"
+ln -sf "$(pwd)/app/build/outputs/apk/release/lib/x86_64/libmediakitandroidhelper.so" "../../../libmpv/src/main/jniLibs/x86_64"
 
-cd ..
+cd ../..
 
 # --------------------------------------------------
 
-cd media_kit/media_kit_native_event_loop
+cd deps/media_kit/media_kit_native_event_loop
+
+flutter create --org com.alexmercerind --template plugin_ffi --platforms=android .
 
 printf "      android:\n        ffiPlugin: true\n" >> pubspec.yaml
 
-flutter create --org com.alexmercerind --template plugin_ffi --platforms=android .
 flutter pub get
 
-cp -a ../../deps/mpv/libmpv/. src/include/
+cp -a ../../mpv/libmpv/. src/include/
 
 cd example/android
 
@@ -57,13 +58,13 @@ zip -r "armeabi-v7a(no-ffmpeg).jar"    lib/armeabi-v7a
 zip -r "x86(no-ffmpeg).jar"            lib/x86
 zip -r "x86_64(no-ffmpeg).jar"         lib/x86_64
 
-mkdir ../../../../../../../../../output
+mkdir ../../../../../../../../../../output
 
-cp *.jar ../../../../../../../../../output
+cp *.jar ../../../../../../../../../../output
 
 md5sum *.jar
 
-cd ../../../../../../../..
+cd ../../../../../../../../..
 
 # --------------------------------------------------
 
