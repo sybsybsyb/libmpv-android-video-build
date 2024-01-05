11138a39f2725153ed6486e647e59c48721eea23
Hitesh Kumar Saini <saini123hitesh@gmail.com>
2024-01-05 20:19:13 +0530

    build: mpv @ 78d4374

--- buildscripts/include/download-deps.sh
+++ buildscripts/include/download-deps.sh
@@ -50,7 +50,7 @@ see <ndk>/sources/third_party/shaderc
 HEREDOC
 
 # mpv
-[ ! -d mpv ] && git clone --depth 1 --branch v$v_mpv https://github.com/mpv-player/mpv.git mpv
+[ ! -d mpv ] && git clone --depth 1 --branch $v_mpv https://github.com/mpv-player/mpv.git mpv
 
 # fftools_ffi
 [ ! -d fftools_ffi ] && git clone --depth 1 --branch main https://github.com/moffatman/fftools-ffi.git fftools_ffi
