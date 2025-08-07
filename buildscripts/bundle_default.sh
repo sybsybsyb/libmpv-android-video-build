2acbe80f06d04a678fed8a6fab7899e0521c6a26
sybsybsyb <yubo.sha@ucdconnect.ie>
2025-08-07 15:18:32 +0800

    Add files via upload

--- buildscripts/bundle_default.sh
+++ buildscripts/bundle_default.sh
@@ -30,55 +30,26 @@ sudo chmod +x gradlew
 
 unzip -o app/build/outputs/apk/release/app-release.apk -d app/build/outputs/apk/release
 
-ln -sf "$(pwd)/app/build/outputs/apk/release/lib/arm64-v8a/libmediakitandroidhelper.so" "../../../libmpv/src/main/jniLibs/arm64-v8a"
-ln -sf "$(pwd)/app/build/outputs/apk/release/lib/armeabi-v7a/libmediakitandroidhelper.so" "../../../libmpv/src/main/jniLibs/armeabi-v7a"
-ln -sf "$(pwd)/app/build/outputs/apk/release/lib/x86/libmediakitandroidhelper.so" "../../../libmpv/src/main/jniLibs/x86"
-ln -sf "$(pwd)/app/build/outputs/apk/release/lib/x86_64/libmediakitandroidhelper.so" "../../../libmpv/src/main/jniLibs/x86_64"
+cp app/build/outputs/apk/release/lib/arm64-v8a/libmediakitandroidhelper.so      ../../prefix/arm64-v8a/usr/local/lib
+cp app/build/outputs/apk/release/lib/armeabi-v7a/libmediakitandroidhelper.so    ../../prefix/armeabi-v7a/usr/local/lib
+cp app/build/outputs/apk/release/lib/x86/libmediakitandroidhelper.so            ../../prefix/x86/usr/local/lib
+cp app/build/outputs/apk/release/lib/x86_64/libmediakitandroidhelper.so         ../../prefix/x86_64/usr/local/lib
 
 cd ../..
 
-# --------------------------------------------------
+mkdir -p tmp/lib/arm64-v8a
+mkdir -p tmp/lib/armeabi-v7a
+mkdir -p tmp/lib/x86
+mkdir -p tmp/lib/x86_64
 
-cd deps/media_kit/media_kit_native_event_loop
+cp prefix/arm64-v8a/usr/local/lib/*.so tmp/lib/arm64-v8a/
+cp prefix/armeabi-v7a/usr/local/lib/*.so tmp/lib/armeabi-v7a/
+cp prefix/x86/usr/local/lib/*.so tmp/lib/x86/
+cp prefix/x86_64/usr/local/lib/*.so tmp/lib/x86_64/
 
-flutter create --org com.alexmercerind --template plugin_ffi --platforms=android .
-
-if ! grep -q android "pubspec.yaml"; then
-  printf "      android:\n        ffiPlugin: true\n" >> pubspec.yaml
-fi
-
-flutter pub get
-
-cp -a ../../mpv/libmpv/. src/include/
-
-cd example
-
-flutter clean
-flutter build apk --release
-
-unzip -o build/app/outputs/apk/release/app-release.apk -d build/app/outputs/apk/release
-
-cd build/app/outputs/apk/release/
-
-# --------------------------------------------------
-
-rm -r lib/*/libapp.so
-rm -r lib/*/libflutter.so
-
-zip -r "default-arm64-v8a.jar"                lib/arm64-v8a
-zip -r "default-armeabi-v7a.jar"              lib/armeabi-v7a
-zip -r "default-x86.jar"                      lib/x86
-zip -r "default-x86_64.jar"                   lib/x86_64
-
-mkdir -p ../../../../../../../../../../output
-
-cp *.jar ../../../../../../../../../../output
+(cd tmp && zip -r ../default-arm64-v8a.jar lib/arm64-v8a)
+(cd tmp && zip -r ../default-armeabi-v7a.jar lib/armeabi-v7a)
+(cd tmp && zip -r ../default-x86.jar lib/x86)
+(cd tmp && zip -r ../default-x86_64.jar lib/x86_64)
 
 md5sum *.jar
-
-cd ../../../../../../../../..
-
-# --------------------------------------------------
-
-zip -r debug-symbols-default.zip prefix/*/lib
-cp debug-symbols-default.zip ../output
--- buildscripts/scripts/libplacebo.sh
+++ buildscripts/scripts/libplacebo.sh
@@ -14,20 +14,13 @@ else
 	exit 255
 fi
 
-unset CC CXX # meson wants these unset
-
+unset CC CXX
 meson setup $build --cross-file "$prefix_dir"/crossfile.txt \
-	--prefer-static \
-	--default-library shared \
-	-Dgpl=false \
-	-Dlibmpv=true \
- 	-Dcplayer=false \
- 	-Dlua=disabled \
-	-Diconv=disabled \
-	-Djavascript=disabled \
- 	-Dmanpage-build=disabled
+	-Dvulkan=disabled -Ddemos=false
 
 ninja -C $build -j$cores
 DESTDIR="$prefix_dir" ninja -C $build install
 
-ln -sf "$prefix_dir"/lib/libmpv.so "$native_dir"
+# add missing library for static linking
+# this isn't "-lstdc++" due to a meson bug: https://github.com/mesonbuild/meson/issues/11300
+${SED:-sed} '/^Libs:/ s|$| -lc++|' "$prefix_dir/lib/pkgconfig/libplacebo.pc" -i
