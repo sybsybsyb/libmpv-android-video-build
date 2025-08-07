faad550770ecf61a63c433f5201c5c29cddac0de
sybsybsyb <yubo.sha@ucdconnect.ie>
2025-08-07 16:15:56 +0800

    revert

--- buildscripts/include/depinfo.sh
+++ buildscripts/include/depinfo.sh
@@ -2,20 +2,19 @@
 
 ## Dependency versions
 
-v_sdk=11076708_latest
-v_ndk=27.1.12297006
-v_sdk_build_tools=35.0.0
+v_sdk=9123335_latest
+v_ndk=25.2.9519653
+v_sdk_build_tools=33.0.2
 
-v_libass=0.17.3
-v_harfbuzz=10.0.1
-v_fribidi=1.0.16
-v_freetype=2-13-3
-v_mbedtls=3.6.1
-v_libplacebo=7.349.0
-v_dav1d=1.4.3
+v_libass=0.17.1
+v_harfbuzz=7.2.0
+v_fribidi=1.0.12
+v_freetype=2-13-0
+v_mbedtls=3.4.0
+v_dav1d=1.2.0
 v_libxml2=2.10.3
-v_ffmpeg=7.1
-v_mpv=0.39.0
+v_ffmpeg=6.0
+v_mpv=78d43740f52db817d98bcf24fb30a76ab6fa13ff
 v_libogg=1.3.5
 v_libvorbis=1.3.7
 v_libvpx=1.13
@@ -38,9 +37,8 @@ dep_harfbuzz=()
 dep_libass=(freetype fribidi harfbuzz)
 dep_lua=()
 dep_shaderc=()
-dep_libplacebo=()
 if [ -n "$ENCODERS_GPL" ]; then
-	dep_mpv=(ffmpeg libass libplacebo fftools_ffi)
+	dep_mpv=(ffmpeg libass fftools_ffi)
 else
-	dep_mpv=(ffmpeg libass libplacebo)
+	dep_mpv=(ffmpeg libass)
 fi
